ca9fc5421e3719fa81a4735810d228ab86ae1d1e
robsoncaetanoferreira <robsoncaetanoferreira2002@gmail.com>
2025-05-07 09:17:52 -0300

    Triangulo.py

---
+++ Triangulo.py
@@ -0,0 +1,8 @@
+base = float(input("Digite o Tamanho da Base:"))
+altura= float( input("Digite a Altura:"))
+
+area = (base * altura) /2
+print("-----------------------------------------------")
+print(f"A AREA DO TRIANGULO É DE: {area} cm")
+print("-----------------------------------------------")
+print("-----PROGRAMA FOI FINALIZADO COM SUCESSO!!!----")
